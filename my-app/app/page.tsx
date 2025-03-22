44ace1be675171d527d6cd0d7a8b4ca8460fdac9
Alexis Penn <lexp.0304@gmail.com>
2025-03-22 17:19:29 -0400

    Crime Form working

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
+import CrimeForm from "./components/CrimeForm"
 
 const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
 
@@ -21,12 +22,12 @@ export default function Home() {
 
   return (
     <div>
-      <h3>HELLO WORLD</h3>
       <GoogleMap
         mapContainerStyle={{ width: "100%", height: "500px" }}
         center={center}
         zoom={10}
       />
+      <CrimeForm onClose={()=> console.log("close")}/>
     </div>
   );
 }
