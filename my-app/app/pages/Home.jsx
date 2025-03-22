2232ff99ece940ec2b846348bf743c5f97d08341
Keith Michelangelo Fernandez <keithfernandez000@gmail.com>
2025-03-22 15:26:40 -0400

    Changes to be committed: 	modified:   my-app/app/components/CrimeMarker.jsx 	modified:   my-app/app/pages/Home.jsx 	new file:   my-app/index.jsx

---
+++ my-app/app/pages/Home.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import CrimeForm from "../components/CrimeForm";
+import CrimeMap from "../components/CrimeMap";
+
+export default function Home() {
+  const [crimes, setCrimes] = useState([]);
+
+  const addCrime = (newCrime) => {
+    const randomCoords = { lat: 40.71 + Math.random() * 0.05, lng: -74.00 + Math.random() * 0.05 };
+    setCrimes([...crimes, { ...newCrime, coordinates: randomCoords }]);
+  };
+
+  return (
+    <div className="flex">
+      <div className="w-1/3 p-4">
+        <CrimeForm onSubmit={addCrime} />
+      </div>
+      <div className="w-2/3">
+        <CrimeMap crimes={crimes} />
+      </div>
+    </div>
+  );
+}
